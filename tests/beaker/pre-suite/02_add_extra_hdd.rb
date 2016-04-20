15b7e584e38a25d8d6adb333dabf327f945d2d91
Matthew Johnson <mrsj@osuosl.org>
2016-04-20 08:40:52 -0700

    Change partition references to work with vagrant

--- tests/beaker/pre-suite/02_add_extra_hdd.rb
+++ tests/beaker/pre-suite/02_add_extra_hdd.rb
@@ -6,15 +6,15 @@ confine_block(:except, :roles => %w{master dashboard database}) do
     agents.each do |agent|
       unless (agent['platform'] =~ /windows/ && agent['platform'] =~ /aix/)
         #keep trying until the hdd is found
-        retry_on(agent, "fdisk -l | grep \"/dev/sdc\"", :max_retries => 420, :retry_interval => 5)
+        retry_on(agent, "fdisk -l | grep \"/dev/sdc1\"", :max_retries => 420, :retry_interval => 5)
 
         #keep trying until the hdd is found
-        retry_on(agent, "fdisk -l | grep \"/dev/sdd\"", :max_retries => 420, :retry_interval => 5)
+        retry_on(agent, "fdisk -l | grep \"/dev/sdc2\"", :max_retries => 420, :retry_interval => 5)
 
         step 'Verify the newly add HDDs recognized:'
         on(agent, "fdisk -l") do |result|
-          assert_match(/\/dev\/sdc/, result.stdout, "Unexpected errors is detected")
-          assert_match(/\/dev\/sdd/, result.stdout, "Unexpected errors is detected")
+          assert_match(/\/dev\/sdc1/, result.stdout, "Unexpected errors is detected")
+          assert_match(/\/dev\/sdc2/, result.stdout, "Unexpected errors is detected")
         end
       end
   end
